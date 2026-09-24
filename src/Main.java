26ed3689825e45f29988008fba42a36f1f66cc6c
mnts1706 <mon.rubio@duocuc.cl>
2026-09-23 22:41:01 -0300

    Se ingresa el segundo ejercicio a revision

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//este comentario es para probar el repositorio
+
+        int edad = 20;
+        double descuento = 0.10;   //Esta variable solo indica el valor del descuento, aún no se calcula;
+
+        double total = 50000;
+        double totalFinal = total - (total * descuento);
+
+        System.out.println("El valor final con el descuento aplicado es de: " +totalFinal);
+
+        /*Esta operacion permite calcular el descuento
+         se resta al valor total que se maneja
+        y así obtenemos el valor final total.*/
+
     }
 }
