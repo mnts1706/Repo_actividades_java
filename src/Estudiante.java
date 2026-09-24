e8403ebd5e5aa794a745d571723a084a96b44ec7
mnts1706 <mon.rubio@duocuc.cl>
2026-09-23 22:58:33 -0300

    Segunda parte del ejercicio numero 2

---
+++ src/Estudiante.java
@@ -0,0 +1,10 @@
+ /*Representa un estudiante registrado dentro del sistema académico.*/
+    public class Estudiante {
+
+         public boolean estaAprobado ( double notaFinal){
+             return notaFinal >= 4.0;
+         }
+         /* este metodo permite saber si el estudiante está aprobado o no.
+          *@param notaFinal nota obtenida por el estudiante.
+          * @return true si la nota es igual o superior a 4.0*/
+     }
